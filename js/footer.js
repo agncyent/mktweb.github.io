fc8fd17d6583c86eb48e3102d03615bf16b37063
Agency entertainment <mktxvirtual@gmail.com>
2026-03-17 09:56:44 +0700

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -5,11 +5,12 @@
 const footer = document.getElementById("footer-placeholder");
 
 if (footer) {
-  fetch("../components/footer.html") // sesuaikan path sesuai struktur folder
-    .then(response => response.text())
-    .then(data => {
-      footer.innerHTML = data;
-    })
-    .catch(error => {
-      console.error("Gagal load footer:", error);
-    });
+  // jika HTML ada di folder /pages/ → pakai ../components/
+  // jika HTML di root → pakai components/
+  let path = location.pathname.includes("/pages/") ? "../components/footer.html" : "components/footer.html";
+
+  fetch(path)
+    .then(res => res.text())
+    .then(data => footer.innerHTML = data)
+    .catch(err => console.error("Gagal load footer:", err));
+}
